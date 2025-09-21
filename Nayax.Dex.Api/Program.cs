f8585f1356cff2846e4e4684f5800bf5922d6d15
Augusto Oliveira <augusto.oliveira.br@outlook.com>
2025-09-21 14:45:34 -0300

    Swagger has been installed

--- Nayax.Dex.Api/Program.cs
+++ Nayax.Dex.Api/Program.cs
@@ -3,21 +3,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Nayax",
+        Version = "v1",
+        Description = "An API for Full-Stack Technical Project"
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
